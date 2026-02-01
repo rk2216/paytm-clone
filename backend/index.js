ddc87244f7fcf07521066002506ef183fa37eb39
rk2216 <rockeshkumawath26@gmail.com>
2026-02-01 14:57:00 +0530

    Add JWT secret and app listen to port 3000

--- backend/index.js
+++ backend/index.js
@@ -10,3 +10,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1', rootRouter);
+
+app.listen(3000, (err) => {
+    console.log('App listening to port 3000');
+    if(err) {
+        console.error(err);
+    }
+})
