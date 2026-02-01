6dd221c99c6d051753228fa73ef17ebf29353156
rk2216 <rockeshkumawath26@gmail.com>
2026-02-01 14:49:10 +0530

    Added CORS for Cross Origin Resource Sharing

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
-const rootRouter = require("./routes/index")
+const cors = require('cors');
+const rootRouter = require("./routes/index");
 
 const app = express();
+
+// Adds headers: Access-Control-Allow-Origin: *
+app.use(cors());
 
 app.use('/api/v1', rootRouter);
