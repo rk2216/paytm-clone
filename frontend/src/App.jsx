7301e3855e9991575ff8e2cd7efd135ea863674d
rk2216 <rockeshkumawath26@gmail.com>
2026-02-17 20:31:24 +0530

    Added frontend routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Signup } from './components/Signup';
+
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/send' element={<Send />} />
+      </Routes>
+    </BrowserRouter>
   )
+}
+
+
+function Signin () {
+  return (<p>Sign in</p>);
+}
+function Dashboard () {
+  return (<p>Dashboard</p>);
+}
+function Send () {
+  return (<p>Send</p>);
 }
 
 export default App
